9ff2d1d17bcf28583153853b5e1bbbe89f83117b
Malachi1k <111375864+Malachi1k@users.noreply.github.com>
2023-10-03 14:37:22 -0400

    02_collections

---
+++ 02_colections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_colections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4a_ajhs_multiuser_gameprogramming_23_24_Malachi1k
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
