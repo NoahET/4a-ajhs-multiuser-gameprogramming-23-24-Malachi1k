6c4d5e53e1084739572a236c49b45c2a366ef319
Malachi1k <111375864+Malachi1k@users.noreply.github.com>
2023-10-04 20:04:51 -0400

    v0.1a

--- 02_colections/Program.cs
+++ 02_colections/Program.cs
@@ -1,4 +1,4 @@
-﻿// 02_collections, Malachi smith, 10-04-23 v0.0
+﻿// 02_collections, Malachi smith, 10-04-23 v0.1a
 using System;
 
 namespace _02_colections
@@ -7,7 +7,22 @@ namespace _02_colections
     {
         static void Main(string[] args)
         {
-            
+            // Collections are variables that can store multiple values in one variable.
+
+            /* Arrays
+            -- Number of elements in an array CANNOT change.
+            -- Contents of elements in an array CAN change.
+            -- Items in the array are called ELEMENTS.
+            -- Arrays are ordered, meaning each item has a fixed position.
+            -- The position is known as the INDEX.
+            -- First element in an array is index 0.
+            */
+
+            // Declaring and Defining an Array
+            string[] breakfastFoods = {"Bacon", "Waffles", "Pankcakes", "Cereal", "Parfait" };
+            int[] testscores = {95, 100, 25, 15, 27, 35};
+            float[] GPA = {3.14f, 2.25f, 1.74f, 1.99f, 0.99f, 4.25f};
+
         }
     }
 }
