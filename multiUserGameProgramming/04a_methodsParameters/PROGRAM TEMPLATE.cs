9e1386514b5868c5f1abf85a7b7b733191293982
Malachi1k <111375864+Malachi1k@users.noreply.github.com>
2023-10-24 14:38:52 -0400

    Create PROGRAM TEMPLATE.cs

---
+++ multiUserGameProgramming/04a_methodsParameters/PROGRAM TEMPLATE.cs
@@ -0,0 +1,35 @@
+// Malachi Smith, Program Template, v0.01
+using System; 
+
+namespace MethodsParameters
+{
+    class MethodsParameters
+    {
+            // METHOD -- named block of code, can used over and over.
+            // All methods have a SIGNATURE that defines their name, parameters, and output.
+            // Example Signature 
+
+            static void MyMethod()
+            {
+                Console.WriteLine("I like mine with lettuce and tomatoes, Heinz 57, and french fried ")
+            }
+            // static -- This method belongs to the current class, it is NOT an object 
+            // void -- This method has no return value.
+
+            static int DoubleUp():
+            {
+                int sum = 0;
+                Console.WriteLine("This method will double a number and return it.\n")
+                Console.WriteLine("Please enter a number on the next line.\n")
+                sum = System.Convert.ToInt32(Console.ReadLine());
+                //sum *= 2;
+                //Console.WriteLine(sum);
+                returm sum;
+            }
+
+            // Methods with Parameters
+            
+            static void Main(string[] args)
+
+    }
+}
